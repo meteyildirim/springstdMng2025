ba55c46ad258e0d34ee6c6f2f79dac00d8fb8788
Mete YILDIRIM <mete.yildirim@boc-group.com>
2025-06-28 17:23:37 +0200

    Conflict exception

--- src/main/java/com/sms/demo/service/validator/UniquePropertyValidator.java
+++ src/main/java/com/sms/demo/service/validator/UniquePropertyValidator.java
@@ -1,5 +1,7 @@
 package com.sms.demo.service.validator;
 
+import com.sms.demo.exception.ConflictException;
+import com.sms.demo.payload.messages.ErrorMessage;
 import com.sms.demo.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,6 +17,18 @@ public class UniquePropertyValidator {
             String ssn,
             String phone,
             String email){
+        if(userRepository.existByUsername(username)){
+            throw new ConflictException(String.format(ErrorMessage.ALREADY_REGISTER_MESSAGE_USERNAME,username));
+        }
+        if(userRepository.existsByEmail(username)){
+            throw new ConflictException(String.format(ErrorMessage.ALREADY_REGISTER_MESSAGE_EMAIL,username));
+        }
+        if(userRepository.existsByPhoneNumber(username)){
+            throw new ConflictException(String.format(ErrorMessage.ALREADY_REGISTER_MESSAGE_PHONE_NUMBER,username));
+        }
+        if(userRepository.existsBySsn(username)){
+            throw new ConflictException(String.format(ErrorMessage.ALREADY_REGISTER_MESSAGE_SSN,username));
+        }
 
     }
 }
